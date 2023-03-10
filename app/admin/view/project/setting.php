edc192621a3afac553213094a992ac68cdb6fb95
daxia <78778443@qq.com>
2023-03-10 14:31:47 +0800

    update

--- app/admin/view/project/setting.php
+++ app/admin/view/project/setting.php
@@ -14,58 +14,59 @@
         {include file='Common/nav' /}
     </div>
     <div class="col-11 tuchu" style="border-radius:5px;">
-
-
         <div style="height:24px;"></div>
         <div class="row">
-            <div class="col-auto accordion" id="accordionExample">
-                <button type="button" class="btn btn-sm btn-outline-primary" data-bs-toggle="modal"
-                        data-bs-target="#exampleModal">添加
-                </button>
-                <table class="table table-hover">
-                    <thead>
-                    <tr>
-                        <th scope="col">域名</th>
-                        <th scope="col">操作</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($mainList as $item) { ?>
-                        <tr>
-                            <td>{$item['domain']}</td>
-                            <td>
-                                <a class="btn btn-sm btn-outline-danger"  href="{:url('project/_del_domain',['id'=>$item['id']])}">删除</a>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                    </tbody>
-                </table>
-            </div>
+            <div class="col-4">
+                <div style="border-radius:10px;border: 1px solid #ccc;padding:10px;">
+                    <div style="padding:10px;">
+                        <span style="color:#666;font-weight: bold;font-size: 16px;">蜻蜓引擎配置</span>
+                    </div>
+                    <div>
+                        <div style="margin-bottom: 10px;">
+                            <span><input class="form-control" type="text" placeholder="请填写蜻蜓Token"></span>
+                        </div>
+                        <div style="margin-bottom: 10px;">
+                            <span><input class="form-control" type="text" placeholder="请填写工作流ID"></span>
+                        </div>
+                        <div style="margin-bottom: 10px;">
+                            <span>
+                                <textarea class="form-control" placeholder='全局变量参数 {"xxx":"xxx"}'></textarea>
+                            </span>
+                        </div>
 
-            <div class="col-auto accordion" id="accordionExample_URL">
-                <button type="button" class="btn btn-sm btn-outline-primary" data-bs-toggle="modal"
-                        data-bs-target="#exampleModal_URL">添加
-                </button>
-                <table class="table table-hover">
-                    <thead>
-                    <tr>
-                        <th scope="col">URL</th>
-                        <th scope="col">操作</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($urlsList as $item) { ?>
-                        <tr>
-                            <td>{$item['url']}</td>
-                            <td>
-                                <a class="btn btn-sm btn-outline-danger"  href="{:url('project/_del_url',['id'=>$item['id']])}">删除</a>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                    </tbody>
-                </table>
+                    </div>
+                </div>
             </div>
-
+            <div class="col-3 accordion" id="accordionExample">
+                <div style="border-radius:10px;border: 1px solid #ccc;padding:10px;">
+                    <div style="padding:10px;">
+                        <span style="color:#666;font-weight: bold;font-size: 16px;">域名配置</span>
+                        <button style="float:right;" type="button" class="btn btn-sm btn-outline-primary"
+                                data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">添加
+                        </button>
+                    </div>
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col" style="color:#ccc;">域名</th>
+                            <th scope="col" style="color:#ccc;">操作</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($mainList as $item) { ?>
+                            <tr>
+                                <td>{$item['domain']}</td>
+                                <td>
+                                    <a class="btn btn-sm btn-outline-danger"
+                                       href="{:url('project/_del_domain',['id'=>$item['id']])}">删除</a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
